31c6b5de0fc23f802d6e03fc2f54a37b36d76a34
Athira Parameswaran s <athira007p@gmail.com>
2023-03-01 11:32:43 +0530

    Add project files.

---
+++ GoogleAuct/Startup.cs
@@ -0,0 +1,27 @@
+﻿////using Amazon.OpsWorks.Model;
+////using FluentAssertions.Common;
+////using IdentityServer4.EntityFramework.Entities;
+////using IdentityServer4.Models;
+////using Microsoft.Extensions.Options;
+
+////namespace GoogleAuct
+////{
+////    public class Startup
+////    {
+////        //Services.AddAuthentication().AddGoogle(options =>
+////        //        {
+////        //    options.ClientId = "881642255142-iqv0cvfmbh2vvvqag62cru0k68pgcgpb.apps.googleusercontent.com";
+////        //    options.ClientSecret = "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";
+////        //        }
+
+////      //dotnet user-secrets set "Authentication:Google:ClientId" "<881642255142-iqv0cvfmbh2vvvqag62cru0k68pgcgpb.apps.googleusercontent.com>";
+////      ////dotnet user-secrets set "Authentication:Google:ClientSecret" "<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>";
+
+////         App.UseGoogleAuthentication(new GoogleOAuth2AuthenticaticationOptions()
+////        {
+////            Client = "881642255142-iqv0cvfmbh2vvvqag62cru0k68pgcgpb.apps.googleusercontent.com";
+////            ClientSecret = "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";
+////        };
+
+////    }
+////}
